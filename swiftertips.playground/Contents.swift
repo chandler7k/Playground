ca7c47fbc7ea8f9ec5a02bc8317e56ad9da47c7d
chandler7k <chandler7k@hotmail.com>
2018-11-21 11:10:26 +0800

    upload switertips

---
+++ swiftertips.playground/Contents.swift
@@ -0,0 +1,255 @@
+import Cocoa
+import Foundation
+
+
+func addOne(num: Int) -> Int{
+    return num + 1
+}
+
+func addTo(_ adder: Int) -> (Int) -> Int{
+    return {
+        num in
+        return num + adder
+    }
+}
+
+let addTen = addTo(10)
+print(addTen(1))
+
+
+protocol TargetAction{
+    func performAction()
+}
+
+
+struct TargetActionWrapper<T: AnyObject>: TargetAction{
+    weak var target: T?
+    let action:(T) -> () -> ()
+    func performAction() {
+        if let t = target{
+            action(t)
+        }
+    }
+}
+
+enum ControlEvent{
+    case touchUpInside
+    case valueChanged
+}
+
+class Control{
+    var actions = [ControlEvent: TargetAction]()
+    func setTarget<T: AnyObject>(_ target: T, action: @escaping (T) -> () -> (), controlEvent: ControlEvent){
+        actions[controlEvent] = TargetActionWrapper(target: target, action: action)
+        
+    }
+    
+}
+
+
+let swiftInt: Int = 1
+let swiftString: String = "hello"
+
+var arrray: [AnyObject] = []
+
+arrray.append(swiftInt as AnyObject)
+arrray.append(swiftString as AnyObject)
+print(arrray)
+
+
+protocol Vehicle{
+    var numberOfWheels: Int {get}
+    var color: String {get set}
+    mutating func changeColor()
+}
+
+
+struct MyCar: Vehicle{
+    let numberOfWheels: Int = 4
+    var color: String = "blue"
+    
+    mutating func changeColor() {
+        color = "red"
+    }
+    
+    
+}
+
+// Iterator  Sequence
+
+class ReverseIterator<T>: IteratorProtocol{
+    typealias Element = T
+    
+    var array: [Element]
+    var currentIndex = 0
+    
+    init(array: [Element]) {
+        self.array = array
+        currentIndex = array.count - 1
+    }
+    
+    func next() -> Element? {
+        if currentIndex < 0{
+            return nil
+        }
+        
+        else{
+            let element = array[currentIndex]
+            currentIndex -= 1
+            return element
+        }
+    }
+}
+
+
+struct ReverseSequence<T>: Sequence{
+    var array: [T]
+    init(array: [T]) {
+        self.array = array
+    }
+    
+    typealias Iterator = ReverseIterator<T>
+    
+    func makeIterator() -> ReverseIterator<T> {
+        return ReverseIterator(array: self.array)
+    }
+    
+}
+
+
+let arr = [0,1,2,3,4]
+
+for i in ReverseSequence(array: arr){
+    print("Index \(i) is \(arr[i])")
+}
+
+// Tuple
+
+func swapT<T>(a: inout T, b: inout T){
+    (a,b) = (b,a)
+}
+var a = 1
+var b = 2
+swap(&a, &b)
+print(a,b)
+
+
+let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
+let (small, large) = rect.divided(atDistance: 20, from: .minXEdge)
+
+print(small)
+
+//MARK: - autoclosure
+
+func logIfTrue(_ predicate: @autoclosure () -> Bool){
+    if predicate(){
+        print("true")
+    }
+}
+
+
+
+logIfTrue(2 > 1)
+
+var level: Int?
+var startLevel = 1
+
+var currentLevel = level ?? startLevel
+
+func dowork(block: () -> ()){
+    block()
+}
+
+dowork {
+    print("work")
+}
+
+
+func doWorkAsync(block: @escaping () -> ()){
+    DispatchQueue.main.async {
+        block()
+    }
+}
+
+
+class S{
+    var foo = "foo"
+    func method1(){
+        dowork {
+            print("foo")
+        }
+        foo = "bar"
+    }
+    
+    func method2(){
+        doWorkAsync {
+            print(self.foo)
+        }
+        
+        foo = "bar"
+    }
+}
+
+S().method1()
+S().method2()
+
+protocol P{
+    func work(b: @escaping () -> ())
+}
+
+class C: P{
+    func work(b: @escaping () -> ()) {
+        DispatchQueue.main.async {
+            print("in c")
+            b()
+        }
+    }
+}
+
+// Optional chaining
+class Toy{
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Pet{
+    var toy: Toy?
+    
+}
+
+class Child{
+    var pet: Pet?
+}
+
+extension Toy{
+    func play(){
+        print("playing")
+    
+    }
+}
+
+let playClosure = {{(child: Child) -> ()? in return child.pet?.toy?.play()}}
+
+
+
+
+// 操作符
+struct Vector2D{
+    var x = 0.0
+    var y = 0.0
+    
+}
+
+let v1 = Vector2D(x: 2.0,y: 1.0)
+let v2 = Vector2D(x: 1.0,y: 4.0)
+let v3 = Vector2D(x: v1.x + v2.x,y: v1.y + v2.y)
+func +(left: Vector2D,right: Vector2D) -> Vector2D{
+    return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    
+}
+
+let v4 = v1 + v2
+
+
