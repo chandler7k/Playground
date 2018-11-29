d76f48634ed01b6dd22acb1cef97b5cec6d7e988
chandler7k <chandler7k@hotmail.com>
2018-11-30 00:37:05 +0800

    try bash
    Signed-off-by: chandler7k <chandler7k@hotmail.com>

--- swiftertips.playground/Contents.swift
+++ swiftertips.playground/Contents.swift
@@ -308,7 +308,50 @@ func foo(){
 
 foo()
 
+enum MyBool: Int{
+    case myTrue, myFalse
+}
 
+extension MyBool: ExpressibleByBooleanLiteral{
+    typealias BooleanLiteralType = Bool
+    init(booleanLiteral value: Bool) {
+        self = value ? .myTrue : .myFalse
+    }
+    
+    
+}
+
+
+let myTrue: MyBool = true
+myTrue.rawValue
+
+class Person: ExpressibleByStringLiteral{
+    typealias StringLiteralType = String
+    typealias ExtendedGraphemeClusterLiteralType = String
+    typealias UnicodeScalarLiteralType = String
+    
+    let name: String
+    init(name value: String){
+        self.name = value
+    }
+
+
+    required convenience init(stringLiteral value: Person.StringLiteralType) {
+        self.init(name: value)
+    }
+    required convenience init(extendedGraphemeClusterLiteral value: Person.ExtendedGraphemeClusterLiteralType) {
+        self.init(name: value)
+    }
+    required convenience init(unicodeScalarLiteral value: Person.UnicodeScalarLiteralType) {
+        self.init(name: value)
+    }
+    
+}
+
+let p: Person = "xiaoming"
+print(p.name)
+
+//>>
 
 
 
