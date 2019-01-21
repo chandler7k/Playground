797e82a7d475eaec0b35a8022f5ff715b52c4335
chandler7k <chandler7k@hotmail.com>
2019-01-22 00:40:24 +0800

    upload solution

--- Nowcoder/10-RotateTheMinistNumberInArray.py
+++ Nowcoder/10-RotateTheMinistNumberInArray.py
@@ -2,4 +2,23 @@
 class Solution:
     def minNumberInRotateArray(self, rotateArray):
         # write code here
+        if len(rotateArray) == 0:
+            return
+        
+        start = 0
+        end = len(rotateArray) - 1
+        mid = (start + end) / 2
+        while rotateArray[start] >= rotateArray[end]:
+            if end - start == 1:
+                mid = end
+                break;
+            
+            mid = (start + end) / 2
+            if rotateArray[mid] >= rotateArray[start]:
+                start = mid
+            elif rotateArray[mid] <= rotateArray[end]:
+                end = mid
+        
+        return rotateArray[mid]
+
         
