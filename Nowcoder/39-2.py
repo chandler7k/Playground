f99d3b55fd5781688e4b070375896db5e3067dee
chandler7k <chandler7k@hotmail.com>
2019-02-28 08:20:10 +0800

    upload

---
+++ Nowcoder/39-2.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def MoreThanHalfNum_Solution(self, numbers):
+        # write code here
+        if len(numbers) <=0:
+            return
+        d = {}
+        res = 0
+        for i in numbers:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for j in d:
+            if d[j] > (len(numbers) / 2):
+                res = j
+        return res
