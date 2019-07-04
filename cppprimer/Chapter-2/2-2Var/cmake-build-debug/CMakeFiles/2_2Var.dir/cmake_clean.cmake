9f4a90640d9b70a14903c39c266c56cd15c70f6e
chandler7k <chandler7k@hotmail.com>
2019-07-04 14:53:24 +0800

    upload

---
+++ cppprimer/Chapter-2/2-2Var/cmake-build-debug/CMakeFiles/2_2Var.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_2Var.dir/main.cpp.o"
+  "2_2Var.pdb"
+  "2_2Var"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_2Var.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cppprimer/Chapter-2/2-2Var/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "2\t\x4d\n" ;
+    return 0;
+}
