46fcd148e6b2c925e6fd2846d819adebff35a20a
chandler7k <chandler7k@hotmail.com>
2019-07-16 21:06:21 +0800

    upload

---
+++ cppprimer/Chapter-2/PointerIntro/cmake-build-debug/CMakeFiles/PointerIntro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointerIntro.dir/main.cpp.o"
+  "PointerIntro.pdb"
+  "PointerIntro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointerIntro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cppprimer/Chapter-2/PointerIntro/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int *p1, *p2;
+    int ival = 21;
+    int *p = &ival;
+    cout << *p << endl;
+    return 0;
+}
