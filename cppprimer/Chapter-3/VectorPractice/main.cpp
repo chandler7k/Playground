f3617d029c097820d31ed892aa4122745778196e
chandler7k <chandler7k@hotmail.com>
2019-08-07 21:27:46 +0800

    upload

--- cppprimer/Chapter-3/VectorPractice/main.cpp
+++ cppprimer/Chapter-3/VectorPractice/main.cpp
@@ -15,18 +15,18 @@ int main() {
     }
     string word;
     vector<string>text;
-    while(cin >>word){
-        if(word == "stop")
-            break;
-        text.push_back(word);
-    }
-
-    for(auto &word: text){
-        for(auto &c: word){
-            c = toupper(c);
-        }
-        cout << word << endl;
-    }
+//    while(cin >>word){
+//        if(word == "stop")
+//            break;
+//        text.push_back(word);
+//    }
+//
+//    for(auto &word: text){
+//        for(auto &c: word){
+//            c = toupper(c);
+//        }
+//        cout << word << endl;
+//    }
 
     auto b = v1.begin(),e = v1.end();
 
@@ -40,6 +40,33 @@ int main() {
 //        cout << i << " ";
 //    }
 //    cout <<endl;
+    string iterString("some string");
+//    if(iterString.begin() != iterString.end()){
+//        auto it = iterString.begin();
+//        *it = toupper(*it);
+//        ++it;
+//    }
+    for(auto it = iterString.begin(); it != iterString.end();++it){
+        *it = toupper(*it);
+    }
+    cout << iterString << endl;
+
+    vector<int>::iterator it;
+    string::iterator it2;
+
+    vector<int>::const_iterator it3;
+    string::const_iterator it4;
+
+    vector<int> noncV;
+    const vector<int> conV;
+
+    auto noncit = noncV.begin();
+    auto conit = conV.begin();
+    conit = conV.cbegin();
+
+//    for(auto it = noncV.begin();it != noncV.end() && !it->empty();++it){
+//
+//    }
 
     return 0;
 }
