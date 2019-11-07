160f4d2843f2e7a4949e8e3aaae6f251f88af7db
chandler7k <chandler7k@hotmail.com>
2019-11-07 15:46:48 +0800

    upload

--- front-end-dev-book/ottergram/scripts/main.js
+++ front-end-dev-book/ottergram/scripts/main.js
@@ -2,7 +2,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]'
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
+var TINNY_EFFECT_CLASS = 'is-tiny';
 
 function setDetails(imageUrl, titleText){
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -25,9 +28,10 @@ function setDetailsFromThumb(thumbnail){
 }
 
 function addThumbClickedHandler(thumb){
-    thumb.addEventListener('click',function (event) {
-        event.preventDetault();
+    thumb.addEventListener('click', function(event){
+        event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     });
 }
 
@@ -37,6 +41,35 @@ function getThumbnailsArray() {
     return thumbnailsArray;
 }
 
+function hideDetails() {
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log('====================================');
+        console.log(event.keyCode);
+        console.log('====================================');
+        if(event.keyCode === ESC_KEY){
+            hideDetails();
+        }
+    });
+}
+
+function showDetails() {
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINNY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINNY_EFFECT_CLASS);
+    });
+}
+
 function initializeEvents() {
     var thumbnials = getThumbnailsArray();
+    thumbnials.forEach(addThumbClickedHandler);
+    addKeyPressHandler();
 }
+
+initializeEvents();
